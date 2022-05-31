dc21baf6bc88e09bd4a4f44a40c0ae7c056db065
thebestliam <106259138+thebestliam@users.noreply.github.com>
2022-05-31 11:05:18 -0400

    Added main ui stuff

---
+++ UI/input.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class rectangle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class input extends Actor
+{
+    
+    
+    
+    public int[] randomInt(int min, int max){
+        
+    }
+    /**
+     * Act - do whatever the rectangle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+    
+      int num = Greenfoot.ask("How many numbers do you want to generate? (5-20)");
+      
+
+    }    
+}
