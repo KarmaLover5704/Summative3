62543a24e45e06edb40ef5d7d335bbc28628510e
thebestliam <106259138+thebestliam@users.noreply.github.com>
2022-06-02 09:59:30 -0400

    Adds the radix code to the UI code
    put Liams code in the main ui class hoping  implement it

--- input.java
+++ input.java
@@ -11,9 +11,7 @@ public class input extends Actor
     
     
     
-    public int[] randomInt(int min, int max){
-        
-    }
+  
     /**
      * Act - do whatever the rectangle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -21,8 +19,8 @@ public class input extends Actor
     public void act() 
     {
     
-      int num = Greenfoot.ask("How many numbers do you want to generate? (5-20)");
+      String num = Greenfoot.ask("How many numbers do you want to generate? (5-20)");
       
-
+      int number = Integer.parseInt(num);
     }    
 }
