e0c8ed4961571f0075614ac6d09b867c8bb80e20
LiamH-dev <106259138+thebestliam@users.noreply.github.com>
2022-06-05 16:37:07 -0400

    Update input.java

--- input.java
+++ input.java
@@ -16,10 +16,18 @@ public class input extends Actor
      * Act - do whatever the rectangle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public void printToScreen(String text,  int x, int y){
+        GreenfootImage letters = new GreenfootImage(text, 25, Color.CYAN, new Color(0, 0, 0, 0));
+        getWorld().getBackground().drawImage(letters, (getWorld().getWidth()-letters.getWidth())/2, y);
+        
+        
+    }
+     
     public void act() 
     {
         
-      
+      int delay = 200;
       int number = 1;
       if(isAsked == false ){
           String num = Greenfoot.ask("How many numbers do you want to generate? (5-20)");
@@ -29,13 +37,16 @@ public class input extends Actor
       
           radixSort.radixSort();
           
-          getWorld().showText(radixSort.getStartingArgs(), 250, 10);
-          
-          
-          getWorld().showText(radixSort.getStage(0), 250, 35);
-          getWorld().showText(radixSort.getStage(1), 250, 60);
-          getWorld().showText(radixSort.getStage(2), 250, 85);
-          getWorld().showText(radixSort.toString(), 250, 110);
+          printToScreen("Original Array: " + radixSort.getStartingArgs(), 250, 10);
+          Greenfoot.delay(delay);
+          printToScreen("First Step: " + radixSort.getStage(0), 250, 35);
+          Greenfoot.delay(delay);
+          printToScreen("Second Step: " + radixSort.getStage(1), 250, 80);
+          Greenfoot.delay(delay);
+          printToScreen("Third Step: " + radixSort.getStage(2), 250, 110);
+          Greenfoot.delay(delay);
+          printToScreen("Sorted array: " + radixSort.toString(), 250, 140);
+          Greenfoot.delay(delay);
           isAsked = true;
       }
       
@@ -43,5 +54,7 @@ public class input extends Actor
       
       
  
-    }    
+    } 
+    
 }
+
